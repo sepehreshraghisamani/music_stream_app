0b4fde41b88a4ac3f39f0338838504ad34ee94c7
sepehreshraghisamani <sepehreshsa@gmail.com>
2025-12-09 12:39:31 -0800

    setting up artists list

--- music/views.py
+++ music/views.py
@@ -2,62 +2,90 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
+import requests
 
-@login_required(login_url='login')
+
+def top_artists():
+
+    url = "https://spotify23.p.rapidapi.com/artists/"
+
+    querystring = {"ids": "2w9zwq3AktTeYYMuhMjju8"}
+
+    headers = {
+        "x-rapidapi-key": "85c6d0c756mshcacc20d7e1f8b95p181220jsn10b7a5dc4832",
+        "x-rapidapi-host": "spotify23.p.rapidapi.com",
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    response_data = response.json()
+    artists_info = []
+    if "artists" in response_data['artists']:
+        for artist in response_data["artists"]:
+            name = artist.get("name", "Unknown Artist")
+            artist_id = artist.get("id", "N/A")
+            avatar_url = artist.get("images", [{}])[0].get("url","")
+            artists_info.append((name, artist_id, avatar_url))
+    return artists_info
+
+
+@login_required(login_url="login")
 def index(request):
-    return render(request,'index.html')
+    artists_info = top_artists()
+    print(artists_info)
+    context = {
+        "artists_info": artists_info,
+    }
+    return render(request, "index.html", context)
+
 
 def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
         if password == password2:
             if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username Taken')
-                return redirect('signup')
+                messages.info(request, "Username Taken")
+                return redirect("signup")
             elif User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Taken')
-                return redirect('signup')
+                messages.info(request, "Email Taken")
+                return redirect("signup")
             else:
-                user = User.objects.create_user(username=username, password=password, email=email)
+                user = User.objects.create_user(
+                    username=username, password=password, email=email
+                )
                 user.save()
-                messages.info(request, 'User Created')
+                messages.info(request, "User Created")
                 user_login = auth.authenticate(username=username, password=password)
                 auth.login(request, user_login)
-                return redirect('/')
+                return redirect("/")
 
         else:
-            messages.info(request, 'Passwords do not match')
-            return redirect('signup')
-
+            messages.info(request, "Passwords do not match")
+            return redirect("signup")
 
     else:
-        return render(request,'signup.html')
+        return render(request, "signup.html")
+
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            return redirect("/")
         else:
-            messages.info(request, 'Invalid Credentials')
-            return redirect('login')
+            messages.info(request, "Invalid Credentials")
+            return redirect("login")
     else:
 
-         return render(request,'login.html')
+        return render(request, "login.html")
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def logout(request):
     auth.logout(request)
-    return redirect('login')
-
-
-
-
-
+    return redirect("login")
